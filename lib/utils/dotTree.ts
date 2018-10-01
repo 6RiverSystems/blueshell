ba6c747050be4ee88d6a7ab378da68deb8482907
Shockolate <tarmstrong@6river.com>
2018-10-01 14:04:17 -0400

    fix: renaming of variables FS-4407

--- lib/utils/dotTree.ts
+++ lib/utils/dotTree.ts
@@ -53,11 +53,11 @@ function getColor<S extends BlueshellState, E>(node: Base<S, E>, state?: S): str
 }
 
 function getNodeId<S extends BlueshellState, E>(node: Base<S, E>): string {
-	const cheat: any = node;
-	if (!cheat.magicTreeId) {
-		cheat.magicTreeId = `n${v4().replace(/\-/g, '')}`;
+	const anyfiedNode: any = node;
+	if (!anyfiedNode.__nodeId) {
+		anyfiedNode.__nodeId = `n${v4().replace(/\-/g, '')}`;
 	}
-	return cheat.magicTreeId;
+	return anyfiedNode.__nodeId;
 }
 
 function getLabel<S extends BlueshellState, E>(node: Base<S, E>): string {
