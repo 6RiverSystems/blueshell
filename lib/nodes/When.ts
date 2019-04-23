f1a0f4e1091fedc5098dc2a278c4a3c995c969d1
Tony Gaetani <tony.gaetani@gmail.com>
2019-04-22 23:41:49 -0700

    feat: add When Composite node

---
+++ lib/nodes/When.ts
@@ -0,0 +1,44 @@
+import {Composite} from './Composite';
+import {BlueshellState} from './BlueshellState';
+import {resultCodes as rc, ResultCode} from '../utils/resultCodes';
+import {Base} from './Base';
+
+export interface Conditional<S, E> {
+	(state: S, event: E): boolean;
+}
+
+/**
+ * When Conditional Composite Node.
+ *
+ * If `conditional(state: S, event: E)` returns true,
+ * control is passed to the consequent node.
+ */
+export class When<S extends BlueshellState, E> extends Composite<S, E> {
+	constructor(name: string,
+	            private readonly conditional: Conditional<S, E>,
+							private readonly consequent: Base<S, E>,
+							private readonly result: ResultCode = rc.SUCCESS,
+							latched: boolean = false) {
+		super(name, [consequent], latched);
+	}
+
+	/**
+	 * If `conditional` resolves to `true`, then the `consequent` node handles the event.
+	 * Otherwise, this node resolves to `SUCCESS` or the provided result
+	 * @override
+	 * @param state The state when the event occured.
+	 * @param event The event to handle.
+	 * @param i The child index, which is always 0
+	 */
+	handleChild(state: S, event: E, i: number) {
+		if (this.conditional(state, event)) {
+			return this.consequent.handleEvent(state, event);
+		} else {
+			return this.result;
+		}
+	}
+
+	get symbol() {
+		return '?!';
+	}
+}
