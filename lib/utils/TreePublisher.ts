e472d36cf53fccddd8b133a48c01730eac94bc74
Jeff Kreis <jkreis@6river.com>
2019-11-18 12:36:00 -0500

    fix: renaming method and simplify typing of static

--- lib/utils/TreePublisher.ts
+++ lib/utils/TreePublisher.ts
@@ -1,13 +1,13 @@
 import {BlueshellState} from '../nodes/BlueshellState';
 
 export interface TreePublisher<B extends BlueshellState, V> {
-	maybePublishTree(state: B, event: V, topLevel: boolean): void;
+	publishTree(state: B, event: V, topLevel: boolean): void;
 	configure(options: object): void;
 }
 
 export class TreeNonPublisher implements TreePublisher<any, any> {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	maybePublishTree(_state: BlueshellState, _event: any, _topLevel: boolean) {}
+	publishTree(_state: BlueshellState, _event: any, _topLevel: boolean) {}
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	configure(_options: object) {}
 }
