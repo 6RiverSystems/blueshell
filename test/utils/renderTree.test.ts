99b74eead90e22fba32cf24064a3878e5a5d1cd3
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2020-01-06 09:54:40 -0500

    fix: update dependencies for security issues
    and cleanup some eslint noise

--- test/utils/renderTree.test.ts
+++ test/utils/renderTree.test.ts
@@ -145,7 +145,7 @@ describe('renderTree', function() {
 			assert.notOk(a.includes(rc.FAILURE));
 			assert.notOk(a.includes(rc.RUNNING));
 			assert.notOk(a.includes(rc.ERROR));
-			console.log(a);
+			// console.log(a);
 
 			done();
 		});
@@ -174,7 +174,7 @@ describe('renderTree', function() {
 			];
 
 			assertWordsInString(a, expectedWords);
-			console.log(a);
+			// console.log(a);
 		});
 	});
 
@@ -190,6 +190,7 @@ describe('renderTree', function() {
 			assert.notOk(dotString.includes('fillcolor="#984ea3"')); // FAILURE
 			assert.notOk(dotString.includes('fillcolor="#377eb8"')); // RUNNING
 			assert.notOk(dotString.includes('fillcolor="#e41a1c"')); // ERROR
+			// eslint-disable-next-line no-console
 			console.log(dotString);
 			assert.doesNotThrow(function() {
 				parse(dotString);
@@ -208,6 +209,7 @@ describe('renderTree', function() {
 			const result = renderTree.toDotString(waitAi, state);
 
 			assert.ok(result);
+			// eslint-disable-next-line no-console
 			console.log(result);
 			assert.doesNotThrow(function() {
 				parse(result);
@@ -217,6 +219,7 @@ describe('renderTree', function() {
 		it('should generate a digraph with custom node', function(done) {
 			const customLSelector = new CustomLatchedSelector();
 			const dotString = renderTree.toDotString(customLSelector);
+			// eslint-disable-next-line no-console
 			console.log(dotString);
 			assert.doesNotThrow(function() {
 				parse(dotString);
