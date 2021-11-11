3f9056569a93f058379c7d5b6033a67fe9869884
Tim Deignan <tdeignan@6river.com>
2021-11-11 16:05:47 -0500

    fix: add break bit, override _afterEvent instead of decorateResult

--- lib/nodes/decorators/While.ts
+++ lib/nodes/decorators/While.ts
@@ -1,7 +1,11 @@
-import {ResultCode, BlueshellState, BaseNode, rc, Conditional} from '../../models';
+import {ResultCode, BlueshellState, BaseNode, rc, Conditional, NodeStorage} from '../../models';
 import {Action} from '../Base';
 import {Decorator} from '../Decorator';
 import {clearChildEventSeen} from '../Parent';
+
+interface WhileNodeStorage extends NodeStorage {
+	break?: boolean,
+}
 
 /**
  * Given a conditional, have the child Node handle an event given a while condition
@@ -19,27 +23,29 @@ export class While<S extends BlueshellState, E> extends Decorator<S, E> {
 	}
 
 	protected decorateCall(handleEvent: (state: S, event: E) => ResultCode, state: S, event: E) {
-		const storage = this.getNodeStorage(state);
+		const storage: WhileNodeStorage = this.getNodeStorage(state);
 
 		if (storage.running || this.conditional(state, event)) {
 			return handleEvent(state, event);
 		} else {
+			storage.break = true;
 			return this.defaultResult;
 		}
 	}
 
-	protected decorateResult(res: ResultCode, state: S, event: E): ResultCode {
-		if (this.conditional(state, event)) {
+	protected _afterEvent(res: ResultCode, state: S, event: E): ResultCode {
+		res = super._afterEvent(res, state, event);
+
+		const storage: WhileNodeStorage = this.getNodeStorage(state);
+
+		if (res !== rc.RUNNING && !storage.break) {
 			Action.treePublisher.publishResult(state, event, false);
 			clearChildEventSeen(this, state);
 			return this.handleEvent(state, event);
 		} else {
+			storage.break = undefined;
 			return res;
 		}
-	}
-
-	get latched() {
-		return true;
 	}
 
 	get symbol(): string {
