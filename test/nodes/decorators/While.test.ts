9a0cc8c27caa380ff2c05e3b087cb56cd881a8a9
Tim Deignan <tdeignan@6river.com>
2021-11-12 01:42:00 -0500

    feat: update test

--- test/nodes/decorators/While.test.ts
+++ test/nodes/decorators/While.test.ts
@@ -1,61 +1,152 @@
 import {assert} from 'chai';
-import {rc} from '../../../lib';
-import * as Behavior from '../../../lib';
-import {DroneState} from '../test/DroneActions';
+import {Action, BlueshellState, rc, ResultCode, decorators} from '../../../lib';
 
-const Action = Behavior.Action;
-const While = Behavior.decorators.While;
-
-class CountUntil extends Action<DroneState, number> {
+class TestAction extends Action<TestState, number> {
 	public eventCount: number = 0;
+	private currentSequenceIdx: number = 0;
 
-	onEvent(state: DroneState, event: number): Behavior.ResultCode {
+	constructor(
+		name?: string,
+		private readonly sequence: {rc: ResultCode, inc: boolean}[] = [{rc: rc.SUCCESS, inc: true}],
+	) {
+		super(name);
+	}
+
+	onEvent(state: TestState, event: number): ResultCode {
 		this.eventCount++;
-		state.flares++;
-		return rc.SUCCESS;
+
+		const sequenceEntry = this.sequence[this.currentSequenceIdx];
+
+		if (sequenceEntry.inc) {
+			state.counter++;
+		}
+
+		this.incrementCurrentResultSequenceIdx();
+		return sequenceEntry.rc;
+	}
+
+	private incrementCurrentResultSequenceIdx(): void {
+		if (++this.currentSequenceIdx >= this.sequence.length) {
+			this.currentSequenceIdx = 0;
+		}
 	}
 }
 
+interface TestState extends BlueshellState {
+	counter: number,
+}
+
 describe('While', function() {
-	it('returns defaultResult when the condition is alreay met', function() {
-		const countUntil = new CountUntil();
-
-		const uut = new While(
-			'test',
-			countUntil,
-			(state: DroneState, event: number) => {
-				return state.flares < 1;
-			},
-			rc.FAILURE,
-		);
-
-		const state = new DroneState();
-		state.flares = 1;
-
-		const result = uut.handleEvent(state, 0);
-
-		assert.strictEqual(rc.FAILURE, result);
-		assert.strictEqual(state.flares, 1);
-		assert.strictEqual(countUntil.eventCount, 0);
-	});
-
 	it('executes the child action until condition is met and returns the final result', function() {
-		const countUntil = new CountUntil();
+		const testAction = new TestAction();
 
-		const uut = new While(
-			'test',
-			countUntil,
-			(state: DroneState, event: number) => {
-				return state.flares < 2;
+		const uut = new decorators.While<TestState, number>(
+			'uut',
+			testAction,
+			(state: TestState, event: number) => {
+				return state.counter < 3;
 			},
 		);
 
-		const state = new DroneState();
+		const state: TestState = {
+			__blueshell: {},
+			counter: 0,
+		};
 
 		const result = uut.handleEvent(state, 0);
 
 		assert.strictEqual(rc.SUCCESS, result);
-		assert.strictEqual(state.flares, 2);
-		assert.strictEqual(countUntil.eventCount, 2);
+		assert.strictEqual(state.counter, 3);
+		assert.strictEqual(testAction.eventCount, 3);
+	});
+
+	it('returns defaultResult when the condition is already met', function() {
+		const testAction = new TestAction();
+
+		const uut = new decorators.While<TestState, number>(
+			'uut',
+			testAction,
+			(state: TestState, event: number) => {
+				return state.counter < 1;
+			},
+			rc.FAILURE,
+		);
+
+		const state: TestState = {
+			__blueshell: {},
+			counter: 1,
+		};
+
+		const result = uut.handleEvent(state, 0);
+
+		assert.strictEqual(rc.FAILURE, result);
+		assert.strictEqual(state.counter, 1);
+		assert.strictEqual(testAction.eventCount, 0);
+	});
+
+	it('defaults defaultResult to SUCCESS', function() {
+		const testAction = new TestAction();
+
+		const uut = new decorators.While<TestState, number>(
+			'uut',
+			testAction,
+			(state: TestState, event: number) => {
+				return state.counter < 1;
+			},
+		);
+
+		const state: TestState = {
+			__blueshell: {},
+			counter: 1,
+		};
+
+		const result = uut.handleEvent(state, 0);
+
+		assert.strictEqual(rc.SUCCESS, result);
+		assert.strictEqual(state.counter, 1);
+		assert.strictEqual(testAction.eventCount, 0);
+	});
+
+	it('latches', function() {
+		const testAction = new TestAction(
+			'testAction',
+			[
+				{rc: rc.RUNNING, inc: false},
+				{rc: rc.SUCCESS, inc: true},
+			],
+		);
+
+		const uut = new decorators.While<TestState, number>(
+			'uut',
+			testAction,
+			(state: TestState, event: number) => {
+				return state.counter < 3;
+			},
+		);
+
+		const state: TestState = {
+			__blueshell: {},
+			counter: 0,
+		};
+
+		let result = uut.handleEvent(state, 0);
+		assert.strictEqual(rc.RUNNING, result);
+		assert.strictEqual(state.counter, 0);
+		assert.strictEqual(testAction.eventCount, 1);
+
+		result = uut.handleEvent(state, 0);
+		assert.strictEqual(rc.RUNNING, result);
+		assert.strictEqual(state.counter, 1);
+		assert.strictEqual(testAction.eventCount, 3);
+
+		result = uut.handleEvent(state, 0);
+		assert.strictEqual(rc.RUNNING, result);
+		assert.strictEqual(state.counter, 2);
+		assert.strictEqual(testAction.eventCount, 5);
+
+		result = uut.handleEvent(state, 0);
+		assert.strictEqual(rc.SUCCESS, result);
+		assert.strictEqual(state.counter, 3);
+		assert.strictEqual(testAction.eventCount, 6);
 	});
 });
