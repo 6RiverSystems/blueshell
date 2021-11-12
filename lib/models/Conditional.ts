ebc3308dbd6c6f49e165038829b6902307411bd2
Tim Deignan <tdeignan@6river.com>
2021-11-12 11:29:11 -0500

    feat: comment on conditional

--- lib/models/Conditional.ts
+++ lib/models/Conditional.ts
@@ -1,3 +1,6 @@
+/**
+ * Used in several components for flow control purposes. Must be a pure function with no side-effects.
+ */
 export interface Conditional<S, E> {
 	(state: S, event: E): boolean;
 }
--- lib/models/ConditionalWithResult.ts
+++ lib/models/ConditionalWithResult.ts
@@ -1,5 +1,8 @@
 import {ResultCode} from './ResultCode';
 
+/**
+ * Used in several components for flow control purposes. Must be a pure function with no side-effects.
+ */
 export interface ConditionalWithResult<S, E> {
 	(state: S, event: E, res: ResultCode): boolean;
 }
